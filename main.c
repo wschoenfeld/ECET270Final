e835ba614fd136c7f2515e3342ad24a9abc3fc0d
joshkulick <82619038+joshkulick@users.noreply.github.com>
2022-11-01 16:54:46 -0400

    Update main.c
    Reformat

--- main.c
+++ main.c
@@ -34,29 +34,29 @@ void Receiver_ModeReturn(uint8_t RecieverInput)
 					
                 EnableTracker ^= 1 << 7; //Toggle IR Speed enable bit
                 break;
-		case 2 : //Autonomous E Brake
-				if (ClearHolds & (1 << 6)) //If Autonomous E Brake was activated clear it
-					ClearHolds ^= 1 << 6; //Clear hold
-				else //if break wasn't engaged
-					EnableTracker ^= 1 << 6; //Toggle Autonomous E Brake enable bit
-				break;
-		case 3 : //Adaptive Cruise
-				EnableTracker ^= 1 << 5; //Toggle Adaptive Cruise enable bit
-				break;
-		case 4 : //Backup/ Rear Collision Avoidance
-				if (ClearHolds & (1 << 4)) //If Backup/ Rear Collision Avoidance was activated clear it
-					ClearHolds ^= 1 << 4; //Clear hold
-				else //if Backup/ Rear Collision Avoidance wasn't engaged
-					EnableTracker ^= 1 << 4; //Toggle Backup/ Rear Collision Avoidance enable bit
-				break;
-		case 5 : //BlindSpot Detection
-				EnableTracker ^= 1 << 3; //Toggle BlindSpot Detection enable bit
-				break;
-		case 6 : //Anti-Roller
-				EnableTracker ^= 1 << 2; //Toggle Anti-Roller enable bit
-				break;
-		case 7 : //Automatic Parallel Parking
-				EnableTracker ^= 1 << 1; //Toggle Automatic Parallel Parking enable bit
-				break;
+	case 2 : //Autonomous E Brake
+			if (ClearHolds & (1 << 6)) //If Autonomous E Brake was activated clear it
+				ClearHolds ^= 1 << 6; //Clear hold
+			else //if break wasn't engaged
+				EnableTracker ^= 1 << 6; //Toggle Autonomous E Brake enable bit
+			break;
+	case 3 : //Adaptive Cruise
+			EnableTracker ^= 1 << 5; //Toggle Adaptive Cruise enable bit
+			break;
+	case 4 : //Backup/ Rear Collision Avoidance
+			if (ClearHolds & (1 << 4)) //If Backup/ Rear Collision Avoidance was activated clear it
+				ClearHolds ^= 1 << 4; //Clear hold
+			else //if Backup/ Rear Collision Avoidance wasn't engaged
+				EnableTracker ^= 1 << 4; //Toggle Backup/ Rear Collision Avoidance enable bit
+			break;
+	case 5 : //BlindSpot Detection
+			EnableTracker ^= 1 << 3; //Toggle BlindSpot Detection enable bit
+			break;
+	case 6 : //Anti-Roller
+			EnableTracker ^= 1 << 2; //Toggle Anti-Roller enable bit
+			break;
+	case 7 : //Automatic Parallel Parking
+			EnableTracker ^= 1 << 1; //Toggle Automatic Parallel Parking enable bit
+			break;
     }
 }
