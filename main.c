70b056bfa66e5eedd9d1dc4d9fe81f5281d95e3a
joshkulick <82619038+joshkulick@users.noreply.github.com>
2022-11-01 16:52:00 -0400

    Update main.c
    Main.c framework + case structure for remote

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ volatile uint8_t EnableTracker; //(IR Speed Controller, Autonomous E brake, Adap
 volatile uint8_t ClearHolds; //(empty, Autonomous E brake, empty, Backup/ Rear Collision Avoidance, empty, empty, empty, empty)
 
 //Function Prototypes
-uint8_t Receiver_ModeReturn(uint8_t EnableTracker, uint8_t ModeReturn);
+void Receiver_ModeReturn(uint8_t RecieverInput); //Reciever Input is D1..D4. Exclude VT (interrupt pin)
 
 int main(void)
 {
@@ -26,5 +26,37 @@ int main(void)
     }
 }
 
-uint8_t Receiver_ModeReturn(uint8_t EnableTracker, uint8_t ModeReturn)
-
+void Receiver_ModeReturn(uint8_t RecieverInput)
+{
+	switch (RecieverInput)
+    {
+        case 1 : //IR Speed Controller
+					
+                EnableTracker ^= 1 << 7; //Toggle IR Speed enable bit
+                break;
+		case 2 : //Autonomous E Brake
+				if (ClearHolds & (1 << 6)) //If Autonomous E Brake was activated clear it
+					ClearHolds ^= 1 << 6; //Clear hold
+				else //if break wasn't engaged
+					EnableTracker ^= 1 << 6; //Toggle Autonomous E Brake enable bit
+				break;
+		case 3 : //Adaptive Cruise
+				EnableTracker ^= 1 << 5; //Toggle Adaptive Cruise enable bit
+				break;
+		case 4 : //Backup/ Rear Collision Avoidance
+				if (ClearHolds & (1 << 4)) //If Backup/ Rear Collision Avoidance was activated clear it
+					ClearHolds ^= 1 << 4; //Clear hold
+				else //if Backup/ Rear Collision Avoidance wasn't engaged
+					EnableTracker ^= 1 << 4; //Toggle Backup/ Rear Collision Avoidance enable bit
+				break;
+		case 5 : //BlindSpot Detection
+				EnableTracker ^= 1 << 3; //Toggle BlindSpot Detection enable bit
+				break;
+		case 6 : //Anti-Roller
+				EnableTracker ^= 1 << 2; //Toggle Anti-Roller enable bit
+				break;
+		case 7 : //Automatic Parallel Parking
+				EnableTracker ^= 1 << 1; //Toggle Automatic Parallel Parking enable bit
+				break;
+    }
+}
